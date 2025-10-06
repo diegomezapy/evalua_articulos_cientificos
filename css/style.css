be9f1e42a922e7a5c6f6bdc7f7e04fb310c02204
Diego Meza <67208702+diegomezapy@users.noreply.github.com>
2025-10-06 09:42:25 -0300

    Create style.css

---
+++ css/style.css
@@ -0,0 +1,9 @@
+scientific-article-evaluator/
+├── index.html
+├── css/
+│   └── style.css
+├── js/
+│   ├── app.js
+│   ├── pdf.worker.min.js (PDF.js worker)
+│   └── xlsx.full.min.js (SheetJS)
+└── README.md
